8311607d53d1e4fb94a50fccaaca3e97bf2c0155
BUZZ3\Buzz <yugiry>
2026-05-12 10:18:32 +0900

    CSVの問題を15個増やした

--- word_gear/Assets/motofuji/Script/Show_Commercial_M.cs
+++ word_gear/Assets/motofuji/Script/Show_Commercial_M.cs
@@ -6,6 +6,8 @@ public class Show_Commercial_M : MonoBehaviour
     public GameObject cm_canvas;
     public int play_time = 0;
     public int play_cm_time = 3;
+    int show_cm_time = 120;
+    int show_cm;
 
     private void Awake()
     {
@@ -18,5 +20,22 @@ public class Show_Commercial_M : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        cm_canvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(play_time == play_cm_time)
+        {
+            show_cm = 0;
+            play_time = 0;
+            cm_canvas.SetActive(true);
+        }
+    }
+
+    public void PlayGame()
+    {
+        play_time++;
     }
 }
